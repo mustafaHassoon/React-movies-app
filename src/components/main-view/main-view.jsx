ca11914deb62189ecce3a2fbcac2d45c1fa10ca7
mustafaHassoon <mustafahassoon@gmail.com>
2021-03-02 00:48:41 +0100

    staging all files to be pushed to remote

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import axios from 'axios';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+
+export class MainView extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: null,
+      selectedMovie: null,
+    };
+  }
+  // One of the "hooks" available in a React Component
+  componentDidMount() {
+    axios
+      .get('https://shrouded-beach-69227.herokuapp.com/movies')
+      .then((response) => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  onMovieClick(movie) {
+    this.setState({
+      selectedMovie: movie
+    });
+  }
+
+
+  render() {
+    const { movies, selectedMovie } = this.state;
+
+    // Before the movies have been loaded
+    if (!movies) return <div className="main-view" />;
+
+    return (
+      <div className="main-view">
+        {selectedMovie
+          ? <MovieView movie={selectedMovie} />
+          : movies.map(movie => (
+            <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)} />
+          ))
+        }
+      </div>
+    );
+  }
+}
